8d7fe25d4a49e995e304619483d400730cab02c8
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-05-28 14:02:07 -0400

    Add Porto Alegre as a marker with an info window.

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -11,11 +11,39 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//Referenced to https://developers.google.com/maps/documentation/javascript/examples
+
  
- function initMap() {
+function initMap() {
     var poa = {lat: -30.035, lng: -51.218};
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
         center: poa
     });
-}     
+
+    var poaString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Porto Alegre</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Porto Alegre</b> is the city I was born in.</p>'+
+    '<p>For more, visit <a href="https://en.wikipedia.org/wiki/Porto_Alegre">'+
+    'https://en.wikipedia.org/wiki/Porto_Alegre</a>.</p>'+
+    '</div>'+
+    '</div>';
+    
+    var poaMarker = new google.maps.Marker({
+        position: poa,
+        map: map,
+        title: 'Porto Alegre'
+    });
+
+    var poaInfo = new google.maps.InfoWindow({
+        content: poaString
+    });
+    
+    poaMarker.addListener('click', function() {
+        poaInfo.open(map, poaMarker);
+    });
+}
