64f9f47ce1a4d98f378dd69cba63f5750ad2c2c8
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-06-01 12:53:07 -0400

    Add button instead of onload.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,11 +22,13 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
-public class DataServlet extends HttpServlet {
+public final class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String hello = "Hello Bernardo!";
+
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Bernardo!");
+    response.getWriter().println(hello);
   }
 }
