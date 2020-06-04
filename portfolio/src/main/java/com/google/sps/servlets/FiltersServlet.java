990ad982386eceb145a5b9490721de500bea1143
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-06-04 19:20:02 +0000

    Create filters servlet and write its dopost function that handles updating filters.

---
+++ portfolio/src/main/java/com/google/sps/servlets/FiltersServlet.java
@@ -0,0 +1,70 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import com.google.gson.Gson;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.sps.data.Comment;
+import com.google.sps.data.AllComments;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
+@WebServlet("/filters")
+public final class FiltersServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Get the filter input from the form.
+        String filter = getParameter(request, "filter-comments", null);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query queryAllComments = new Query("AllComments");
+        PreparedQuery resultsAllComments = datastore.prepare(queryAllComments);
+        Iterator<Entity> iterAllComments = resultsAllComments.asIterator();
+        Entity entityAllComments = iterAllComments.next();
+
+        // Update the filter property
+        entityAllComments.setProperty("filter", filter);
+
+        // Add the updated entity back in the datastore
+        datastore.put(entityAllComments);
+
+        response.sendRedirect("/contact.html");
+        return;
+    }
+
+    /** Returns the desired parameter entered by the user, or null if the user input was invalid. */
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+        // Get the input from the form.
+        String value = request.getParameter(name);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return value;
+    }
+}
