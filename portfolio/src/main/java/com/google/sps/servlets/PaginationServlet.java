9e177320b8deab7c03b36549cd19c05edbdb3a2e
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-06-04 23:16:14 +0000

    Edits to increase readability of PaginationServlet.java.

--- portfolio/src/main/java/com/google/sps/servlets/PaginationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PaginationServlet.java
@@ -37,48 +37,57 @@ public final class PaginationServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("AllComments");
-        PreparedQuery results = datastore.prepare(query);
-        Iterator<Entity> iter = results.asIterator();
-        Entity entity = iter.next();
+        Entity allCommentsEntity = getAllCommentsEntity();
 
-        List<AllComments> info = new ArrayList<>();
-        long total = (long) entity.getProperty("total");
-        long max = (long) entity.getProperty("max");
-        long page = (long) entity.getProperty("page");
-        String filter = (String) entity.getProperty("filter");
-        info.add(new AllComments(total, max, page, filter));
+        // Initialize AllComments object to be passed as a json.
+        List<AllComments> allComments = new ArrayList<>();
+        long total = (long) allCommentsEntity.getProperty("total");
+        long max = (long) allCommentsEntity.getProperty("max");
+        long page = (long) allCommentsEntity.getProperty("page");
+        String filter = (String) allCommentsEntity.getProperty("filter");
+        allComments.add(new AllComments(total, max, page, filter));
         
-        // Convert to json
-        String json = convertToJsonUsingGson(info);
+        // Convert to json.
+        String json = convertToJsonUsingGson(allComments);
         response.setContentType("application/json;");
         response.getWriter().println(json);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("AllComments");
-        PreparedQuery results = datastore.prepare(query);
-        Iterator<Entity> iter = results.asIterator();
-        Entity entity = iter.next();
+        Entity allCommentsEntity = getAllCommentsEntity();
 
+        // Update the page property of AllComments.
         long newPage = Long.parseLong(request.getParameter("i")) + 1;
-        entity.setProperty("page", newPage);
-        datastore.put(entity);
+        allCommentsEntity.setProperty("page", newPage);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(allCommentsEntity);
 
         response.sendRedirect("/contact.html");
         return;
     }
 
-    /**
-    * Converts the comments array  into a JSON string using the Gson library. Note: We first added
-    * the Gson library dependency to pom.xml.
-    */
+    //Converts the comments array  into a JSON string using the Gson library.
     private String convertToJsonUsingGson(List<AllComments> allComments) {
         Gson gson = new Gson();
         String json = gson.toJson(allComments);
         return json;
+    }
+
+    // Accesses the datastore to get the AllComments entity. Returns the entity or null if one does not exist.
+    private Entity getAllCommentsEntity() {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query queryAllComments = new Query("AllComments");
+        PreparedQuery resultsAllComments = datastore.prepare(queryAllComments);
+
+        // Return null if there are no AllComments entity.
+        if (resultsAllComments.countEntities() == 0) {
+            return null;
+        }
+
+        Iterator<Entity> iterAllComments = resultsAllComments.asIterator();
+        Entity allCommentsEntity = iterAllComments.next(); 
+
+        return allCommentsEntity;
     }
 }
