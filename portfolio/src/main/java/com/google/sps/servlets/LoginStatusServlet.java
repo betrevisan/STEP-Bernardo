fc45db6653f0b7e88172263c58c0549fa36e8ac4
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-06-10 18:06:16 +0000

    Redirect user to the same page they were in when logging out.

--- portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -35,8 +35,12 @@ public class LoginStatusServlet extends HttpServlet {
         UserService userService = UserServiceFactory.getUserService();
 
         if (userService.isUserLoggedIn()) {
-            String logoutUrl = userService.createLogoutURL("/index.html");
+            Entity userInfoEntity = getUserInfoEntity();
+            String where = (String) userInfoEntity.getProperty("where");
+
+            String logoutUrl = userService.createLogoutURL(where);
             String username = getUsername(userService.getCurrentUser().getUserId());
+
             if (username == null) {
                 response.getWriter().println("{\"status\": \"True\", \"logoutUrl\": \"" + logoutUrl + "\", \"username\": \"null\"}");
             } else {
@@ -62,5 +66,18 @@ public class LoginStatusServlet extends HttpServlet {
 
         String username = (String) entity.getProperty("username");
         return username;     
+    }
+
+    // Accesses the datastore to get the UserInfo entity. Returns the entity or null if one does not exist.
+    private Entity getUserInfoEntity() {
+        UserService userService = UserServiceFactory.getUserService();
+        String id = userService.getCurrentUser().getUserId();
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Filter queryFilter = new FilterPredicate("id", Query.FilterOperator.EQUAL, id);
+        Query query = new Query("UserInfo").setFilter(queryFilter);
+        PreparedQuery results = datastore.prepare(query); 
+        Entity userInfoEntity = results.asSingleEntity(); 
+
+        return userInfoEntity;
     }
 }
